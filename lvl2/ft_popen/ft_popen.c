47dee224ebeda36ea15ceacecf55b7c455431bc9
AnassanA1 <teamatlasdesign@gmail.com>
2025-11-25 11:11:13 +0100

    this is for the exam04 42

---
+++ lvl2/ft_popen/ft_popen.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int	ft_popen(const char *file, char *const argv[], char type) {
+
+    if (!file || !argv || (type != 'r' && type != 'w'))
+        return (-1);
+    
+    int fd[2];
+    pid_t pid;
+
+    if (pipe(fd) == -1)
+        return (-1);
+
+    pid = fork();
+    if (pid < 0)
+        return (-1);
+    else if (pid == 0)
+    {
+        if (type == 'r')
+        {
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[0]);
+            close(fd[1]);
+        }
+        else
+        {
+            dup2(fd[0], STDIN_FILENO);
+            close(fd[0]);
+            close(fd[1]);
+        }
+        execvp(file, argv);
+        exit(1);
+    }
+    if (type == 'r')
+    {
+        close(fd[1]);
+        return (fd[0]);
+    }
+    else if (type == 'w')
+    {
+        close(fd[0]);
+        return (fd[1]); 
+    }
+    return (-1);
+}
+
+int main() {
+	int fd = ft_popen("wc", (char *const[]){"wc", "-l" , NULL}, 'w');
+
+    char buf[1201];
+    int ret = 0;
+
+    while ((ret = read(fd, buf, 1200)) > 0)
+    {
+        buf[ret] = '\0';
+        write(1, buf, ret);
+    }
+    close(fd);
+    return (0);
+}
